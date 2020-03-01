949e4467b64d167283cac627958ec614711857af
santiago-restrepo <sanpra1208@gmail.com>
2020-03-01 16:15:44 -0500

    Add project skeleton

---
+++ icesi-bank/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    const element = (
+      <div>
+		  <h1>Hello World!</h1>
+      </div>
+    )
+    return element
+  }
+}
+ 
+export default App;
